804948385b75da43b9748519bda6c07da3bb92c0
alexandre <alexandre020602@gmail.com>
2026-04-14 21:15:51 -0300

    Refactor Profile component and enhance RootLayout for authentication routing

--- src/app/(tabs)/profile.tsx
+++ src/app/(tabs)/profile.tsx
@@ -1,11 +1,8 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import { useRouter } from 'expo-router'
-
 
 const Profile = () => {
-
-  const router = useRouter()
+  
   return (
     <View>
       <Text>profile</Text>
